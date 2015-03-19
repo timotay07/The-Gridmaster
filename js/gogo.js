96c1f2249ee22fd899e986a69de339f95661036e
Tony V. <maviola5@gmail.com>
2015-03-18 22:55:13 -0400

    added column classes for different media queries

--- js/gogo.js
+++ js/gogo.js
@@ -6,14 +6,14 @@ $(document).ready(function(){
 
     /*Establishes horizontal rhythm*/
     $body.prepend('<div id="xAxis"></div>');
-    $("#xAxis").prepend('<div class="group fill"></div>');
+    $("#xAxis").prepend('<div class="fill"></div>');
     for (i = 0; i < numCols; i++) {
-        $("#xAxis > div").prepend('<div class="cols"></div>');
+        $("#xAxis > div").prepend('<div class="xcols"></div>');
     }
     $body.prepend('<div id="yAxis"></div>');
     $body.prepend('<div class="fixed"><button id="toggle">Toggle Baseline</button></div>');
     
-    $(".cols").css( "width", colWidth +"%" );
+    $(".xcols").css( "width", colWidth +"%" );
     var screenHeight = screen.height;
     
     $("#xAxis > .fill div").height(screenHeight);
@@ -56,7 +56,7 @@ $(document).ready(function(){
     **nice clean style sheet and folder!
     */
 
-    $("head").append("<style>.cols{float:left;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.fill>.cols{border-right:.8125em solid #fff;border-left:.8125em solid #fff;height:100px;background:#000;width:100%}#yAxis{position:absolute;z-index:-1;top:0;bottom:0;right:0;height:100%;width:100%;opacity:.1}#xAxis{max-width:1000px;width:100%;opacity:.1;z-index:0;margin:auto;height:100%;position:absolute;top:0;right:0;left:0}.fixed{position:fixed;bottom:0;z-index:10}hr{border:1px solid #000;margin:0}hr:last-child{margin:0!important}</style>");
+    $("head").append("<style>.xcols{float:left;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.fill>.xcols{border-right:.8125em solid #fff;border-left:.8125em solid #fff;height:100px;background:#000;width:100%}#yAxis{position:absolute;z-index:-1;top:0;bottom:0;right:0;height:100%;width:100%;opacity:.1}#xAxis{max-width:1000px;width:100%;opacity:.1;z-index:0;margin:auto;height:100%;position:absolute;top:0;right:0;left:0}.fixed{position:fixed;bottom:0;z-index:10}hr{border:1px solid #000;margin:0}hr:last-child{margin:0!important}</style>");
 
 
     /*
@@ -64,4 +64,13 @@ $(document).ready(function(){
     */
 
 
+});
+
+
+//for debuggins
+
+$(document).ready(function(){
+    
+    
+    $(".demo").height(100);
 });
