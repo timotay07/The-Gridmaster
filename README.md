366d506a7ae40fb460cf371b00f1b3c71deed563
Tony V. <maviola5@gmail.com>
2015-03-19 23:22:15 -0400

    Control Panel / Grid Class Name Clean Up
    Control Panel for Grid manipulation was started. Cleaned up class names
    for column sizes and general css.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Go-Go-Framework
 
 This is the beginning of something new!
+
+The Go Go Framework. A structured view of your browser, that enables you to create both horizontal and vertical rhythm.
--- js/object.js
+++ js/object.js
@@ -1,3 +1,37 @@
+$(document).ready(function(){
+	var inputLH = $("input[name=lineHeight]");
+	var lineHeight = Number(inputLH.val());
+	$("#up").click(function(){
+		inputLH.val(lineHeight + 1);
+		lineHeight = Number(inputLH.val());
+	});
+	$("#down").click(function(){
+		inputLH.val(lineHeight - 1);
+		lineHeight = Number(inputLH.val());
+	});
+
+	var toggleGrid = $("#toggle-grid");
+	toggleGrid.click(function(){
+		var toggleVal = toggleGrid.html();
+		if( toggleVal == "Off"){
+			$("#grid-control-panel").css("opacity",0.2);
+			toggleGrid.html("On");
+			//put code here to toggle off grid
+		}
+		if(toggleVal == "On"){
+			$("#grid-control-panel").css("opacity", 1);
+			toggleGrid.html("Off");
+			//put code to toggle on grid
+		}
+	});
+
+});
+
+
+
+
+
+
 var containerWidth = 12;
 var columns;
 
