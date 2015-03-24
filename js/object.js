9c63d4b940d3dddeceb8404d4883afa725a274fb
Tony V. <maviola5@gmail.com>
2015-03-23 23:31:16 -0400

    changed grid color
    for more visibility

--- js/object.js
+++ js/object.js
@@ -20,6 +20,8 @@ $(document).ready(function(){
     
     $("#xAxis > .fill div").height(screenHeight);
     
+
+
     /* Establishes vertical rhythm or "the Baseline" */
     var inputLH = $("input[name=lineHeight]");
 	var lineHeight = Number(inputLH.val());
@@ -30,11 +32,11 @@ $(document).ready(function(){
    //builds yAxis
    function build_yAxis(){
     for (i = 0; i <= lines; i++) {
-    $("#yAxis").prepend("<hr/>");
+    $("#yAxis").prepend('<hr class="latitudes"/>');
         console.log(i);
     }
     
-    $("hr").css("margin-bottom" , adjustedLineHeight);
+    $("hr.latitudes").css("margin-bottom" , adjustedLineHeight);
     
     $("#yAxis").height(screenHeight);
     //remove on production
@@ -45,6 +47,7 @@ $(document).ready(function(){
     /*Call the function for initial build*/
     build_yAxis();
 	
+$body.append('<div id="yAxis"></div>');
 	// controls and inputs for control-panel
 	$("#up").click(function(){
 		inputLH.val(lineHeight + 1);
@@ -81,10 +84,8 @@ $(document).ready(function(){
 
 });
 
-//grid styles!
- $("head").append("<style>.xcols{float:left;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.fill>.xcols{border-right:.8125em solid #fff;border-left:.8125em solid #fff;background:#000;width:100%}#yAxis{position:absolute;z-index:100;top:0;bottom:0;right:0;height:100%;width:100%;opacity:.1}#xAxis{max-width:1000px;width:100%;opacity:.1;z-index:100;margin:auto;height:100%;position:absolute;top:0;right:0;left:0}.fixed{position:fixed;bottom:0;z-index:101}hr{border:1px solid #000;margin:0}hr:last-child{margin:0!important}</style>");
-
-
+//gogo grid-system styles!
+ $("head").append("<style>.xcols{float:left;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.fill>.xcols{border-right:.8125em solid #fff;border-left:.8125em solid #fff;background:#e74c3c;width:100%}#yAxis{position:absolute;z-index:100;top:0;bottom:0;right:0;height:100%;width:100%;opacity:.1}#xAxis{max-width:1000px;width:100%;opacity:.1;z-index:100;margin:auto;height:100%;position:absolute;top:0;right:0;left:0}.fixed{position:fixed;bottom:0;z-index:101}hr.latitudes{border:1px solid #e74c3c;margin:0}hr.latitudes:last-child{margin:0!important}#grid-control-panel>input,button#down,button#toggle-grid,button#up{font-size:1em}#grid-control-panel{font-family:sans-serif;padding:1em;font-size:1em;position:fixed;bottom:0;left:0;z-index:101}label{float:left;text-align:center;line-height:26px;background-color:#000;color:#fff;padding:1em;border:1px solid #fff;width:112.5px;font-size:1em;border-top-left-radius:5px;border-bottom-left-radius:5px}#grid-control-panel>input{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;width:75px;float:left;text-align:center;background-color:#000;border:1px solid #fff;color:#fff;padding:1em;height:60px;font-size:1em}#down,#up{display:block;height:50%;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;background-color:#000;border:1px solid #fff;width:50px;color:#fff;position:relative;cursor:pointer}#tuner{height:60px;position:relative;float:left}#toggle-grid{height:60px;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;padding:1em;border:1px solid #fff;background:#000;color:#fff;width:75px;border-top-right-radius:5px;border-bottom-right-radius:5px;cursor:pointer}button#down:focus,button#toggle-grid:focus,button#up:focus{outline:0}</style>");
 
 
 
